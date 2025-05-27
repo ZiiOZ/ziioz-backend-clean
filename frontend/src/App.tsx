7184ffbde2b41b01d393f244ea1ae7a3ea63825e
ZiiOZ <support@ziioz.com>
2025-05-27 04:25:58 +0000

    Fix App.tsx: remove placeholder and add post detail route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,14 @@ import ZiiPostForm from './components/ZiiPostForm';
 import ZiiPostFeed from './components/ZiiPostFeed';
 import ZiiPostDetail from './components/ZiiPostDetail';
 
-...
-
-<Routes>
-  <Route path="/ziipostform" element={<ZiiPostForm />} />
-  <Route path="/ziiposts" element={<ZiiPostFeed />} />
-  <Route path="/post/:id" element={<ZiiPostDetail />} />
-</Routes>
-
-
-
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<ZiiPostFeed />} />
         <Route path="/ziipostform" element={<ZiiPostForm />} />
         <Route path="/ziiposts" element={<ZiiPostFeed />} />
+        <Route path="/post/:id" element={<ZiiPostDetail />} />
         <Route path="*" element={<div className="p-6 text-center">Page Not Found</div>} />
       </Routes>
     </Router>
