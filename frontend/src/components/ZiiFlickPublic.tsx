4db3cdc1b40b798ff329623a65e6221eac65dc6d
ZiiOZ <support@ziioz.com>
2025-05-27 00:49:37 +0000

    Fully replaced ZiiFlickPublic.tsx to resolve duplicate declaration error

--- frontend/src/components/ZiiFlickPublic.tsx
+++ frontend/src/components/ZiiFlickPublic.tsx
@@ -81,7 +81,14 @@ function ZiiFlickPublic() {
   );
 }
 
+import React from 'react';
+
 export default function ZiiFlickPublic() {
-  return <div>ZiiFlick Public Viewer</div>;
+  return (
+    <div className="p-6 text-center text-2xl font-semibold text-blue-700">
+      ZiiFlick Public Viewer Page
+    </div>
+  );
 }
+
 
