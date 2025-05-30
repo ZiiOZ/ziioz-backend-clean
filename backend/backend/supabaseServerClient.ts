739a45c151dce8c8aeef3e1fefd4a813a35c2d8e
ZiiOZ <support@ziioz.com>
2025-05-30 00:47:37 +0000

    Add supabase.ts frontend + supabaseServerClient.ts backend

--- backend/backend/supabaseServerClient.ts
+++ backend/backend/supabaseServerClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export { supabase };
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
