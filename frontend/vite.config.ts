3b0ef1e5ef7c5ea081f62b2ad17eb56233348af0
ZiiOZ <support@ziioz.com>
2025-06-02 12:29:08 +0000

    ✅ Fix: moved index.html to root of frontend for Vite build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // important to stay at project root
-  publicDir: 'public',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
   },
-})
+});
