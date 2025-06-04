e25b1bc0685ad81ead0c75c899656982c1aaf7f7
ZiiOZ <support@ziioz.com>
2025-06-04 05:13:32 +0000

    Fix: render ZiiPostForm and Feed together on /ziiposts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import ZiiPostForm from './components/ZiiPostForm'
 import ZiiPostFeed from './components/ZiiPostFeed'
 import NotFound from './components/NotFound'
+
+function ZiiPostPage() {
+  return (
+    <div>
+      <ZiiPostForm />
+      <ZiiPostFeed />
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/ziiposts" />} />
-        <Route path="/ziiposts" element={<ZiiPostFeed />} />
+        <Route path="/ziiposts" element={<ZiiPostPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
