798ef3a37b02ed4e59b3b2d8a843c30dbe214a78
ZiiOZ <support@ziioz.com>
2025-06-05 05:01:31 +0000

    ✅ Fix vite config to correctly resolve public/index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'frontend',
+  root: './frontend',
+  publicDir: './frontend/public', // 👈 this tells Vite where your index.html and assets are
   build: {
     outDir: '../dist',
     emptyOutDir: true,
