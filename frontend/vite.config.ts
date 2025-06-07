29105eff11d39d8668c5127ab0fe86d50245c248
ZiiOZ <support@ziioz.com>
2025-06-07 10:16:12 +0000

    Fix: added build script and supabase-js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  root: '.', // ensures it looks at root for index.html
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-});
+    emptyOutDir: true
+  }
+})
