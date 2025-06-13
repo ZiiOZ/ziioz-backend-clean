5ece21e5ebe55f2414b489a9b47c34b375b89ded
ZiiOZ <support@ziioz.com>
2025-06-13 08:40:42 +0000

    🔥 Remove .htaccess to fix routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public', // <-- ensures _redirects gets copied to dist/
+  publicDir: 'public', // must be present
 });
