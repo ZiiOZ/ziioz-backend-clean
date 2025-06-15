63f4a7391959e5f13b55f8d732c07affe09d9795
ZiiOZ <support@ziioz.com>
2025-06-15 10:21:24 +0000

    Fix: correct case for Home import

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+// src/pages/Home.tsx
+export default function Home() {
+  return (
+    <div className="text-center mt-10 text-xl font-semibold text-blue-600">
+      🏠 Welcome to the ZiiOZ Home Page!
+    </div>
+  );
+}
