7753a4db87ce19d352e8c55d5deabd837fbbefee
ZiiOZ <support@ziioz.com>
2025-06-16 09:43:15 +0000

    Force _redirects copy with vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // copies to dist/
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -18,3 +30,4 @@ export default defineConfig({
     },
   },
 });
+
