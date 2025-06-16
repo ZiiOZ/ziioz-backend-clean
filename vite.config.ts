a38a54b339cb04ccea69e95bfdb3e15b433ec100
ZiiOZ <support@ziioz.com>
2025-06-16 10:12:55 +0000

    Ensure _redirects is in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import fs from 'fs'
+
+// Ensure _redirects gets copied to dist
+const copyRedirects = () => {
+  return {
+    name: 'copy-redirects',
+    closeBundle() {
+      const src = resolve(__dirname, 'public/_redirects');
+      const dest = resolve(__dirname, 'dist/_redirects');
+      fs.copyFileSync(src, dest);
+    },
+  };
+};
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.', // Copy to root of `dist/`
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-});
+  plugins: [copyRedirects()],
+})
