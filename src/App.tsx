dcdbc4f26896e28f2ffbcb57b9bdaa4b08b40891
ZiiOZ <support@ziioz.com>
2025-06-17 08:02:57 +0000

    Remove broken LawDashboard import and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// src/App.tsx
-// Add to the top of every .tsx file (especially if older Vite/TypeScript version)
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LawDashboard from "./pages/LawDashboard";
 import Home from "./pages/Home";
 
 function App() {
@@ -10,10 +6,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/lawdashboard" element={<LawDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
