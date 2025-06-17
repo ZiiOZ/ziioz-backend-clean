6ccd798dfa45c041e541232920de42442406c880
ZiiOZ <support@ziioz.com>
2025-06-17 06:51:15 +0000

    Fix build syntax issues and React injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-
 
 export default defineConfig({
-  esbuild: {
-  jsxInject: `import React from 'react'`
-}
   plugins: [react()],
   resolve: {
     alias: {
@@ -18,4 +12,7 @@ export default defineConfig({
   server: {
     host: true,
   },
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  }
 });
