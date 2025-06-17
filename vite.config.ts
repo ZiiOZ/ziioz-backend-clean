b59230fe58f9cd2599899724adcec04bee13d1fc
ZiiOZ <support@ziioz.com>
2025-06-17 10:37:38 +0000

    fix: enable client routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  root: '.', // 👈 this line ensures Vite knows where to find index.html
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
