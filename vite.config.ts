f5477518257c53a2186d05309812fc12d3170133
ZiiOZ <support@ziioz.com>
2025-06-18 00:41:50 +0000

    Final fix: remove static copy, proper _redirects in public

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
