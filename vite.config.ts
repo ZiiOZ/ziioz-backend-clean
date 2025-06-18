a24ce89ca70d85740573691926d4f0cccff43e78
ZiiOZ <support@ziioz.com>
2025-06-18 00:48:24 +0000

    Remove viteStaticCopy plugin to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'  // copies it to the root of /dist/
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
