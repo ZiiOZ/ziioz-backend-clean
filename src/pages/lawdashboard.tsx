02f1b144050fd40c5b7734eaa1ae12f79b0e66c9
ZiiOZ <support@ziioz.com>
2025-06-18 00:25:21 +0000

    Fix: remove rogue <Route> from LawDashboard

--- src/pages/lawdashboard.tsx
+++ src/pages/lawdashboard.tsx
@@ -1,7 +1,5 @@
-// src/pages/LawDashboard.tsx
 import React from "react";
 
-<Route path="/LawDashboard" element={<LawDashboard />} />
 export default function LawDashboard() {
   return (
     <div className="text-center text-2xl font-bold text-red-600 mt-10">
