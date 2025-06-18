f751d37a2e69740fe0525298d2f19bfd6d25c5b6
ZiiOZ <support@ziioz.com>
2025-06-18 01:52:40 +0000

    Fix: Ensure redirects file copied correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+viteStaticCopy({
+  targets: [
+    { src: 'public/_redirects', dest: '.' }, // <- this ensures it's in /dist
+  ]
+})
+
 
 export default defineConfig({
   plugins: [react()],
