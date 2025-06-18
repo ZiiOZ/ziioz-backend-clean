616fe2c8d01909cb54a17fa3c39ed9249130a641
ZiiOZ <support@ziioz.com>
2025-06-18 02:44:11 +0000

    Add rewrites and static routing configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy'; // ✅ THIS is required
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
+    base: "/",
     react(),
     viteStaticCopy({
       targets: [
         {
-          src: 'public/_redirects',
-          dest: '.' // ⬅️ this ensures it lands directly in /dist
+          src: 'public/_redirects', // ⬅️ Must be exact path
+          dest: '.'                // ⬅️ So it goes into dist/
         }
       ]
     })
