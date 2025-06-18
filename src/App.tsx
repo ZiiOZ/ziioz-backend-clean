a821131206127f482588478e3cd207e88e4b164d
ZiiOZ <support@ziioz.com>
2025-06-18 00:12:45 +0000

    Move _redirects to public for Vercel routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import LawDashboard from "./pages/LawDashboard";
+import LawDashboard from "./pages/lawdashboard";
 
 function App() {
   return (
